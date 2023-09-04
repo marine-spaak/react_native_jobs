0c870ef573e7afbf2367c414d7b2e4a3eab411d0
marine-spaak <marine.spaak@gmail.com>
2023-09-04 16:37:49 +0200

    link to api and new hook useFetch

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -6,13 +6,15 @@ import styles from './popularjobs.style';
 import { COLORS, SIZES } from '../../../constants';
 import PopularJobCard from '../../common/cards/popular/PopularJobCard';
 
+import useFetch from '../../../hook/useFetch';
+
 const Popularjobs = () => {
   const router = useRouter();
 
   // Pour la démo, je crée une constante isLoading que je passe en false
   // idem pour error
   const isLoading = false;
-  const error = false; 
+  const error = false;
 
   return (
     <View style={styles.container}>
---
+++ hook/useFetch.js
@@ -0,0 +1,60 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+// Voici comment on gère la variable d'environnement
+// Voir ici et dans le env
+import { RAPID_API_KEY } from '@env';
+const rapidApiKey = RAPID_API_KEY;
+
+const useFetch = (endpoint) => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  // On utilise le code fourni par l'API mais on le dynamise
+  // Grace à la variable "endpoint" que l'on passe en prop
+  const options = {
+    method: 'GET',
+    url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+    // Je passe ma clé API grace à une variable d'environnement
+    headers: {
+      'X-RapidAPI-Key': rapidApiKey,
+      'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
+    },
+    // Toujours pour dynamiser, je passe une "query" aux paramètres
+    // Pour effectuer la recherche sur les params qui m'intéressent
+    params: { ...query },
+  };
+
+  const fetchData = async () => {
+    // Quand on commence à chercher, le loading commence
+    setIsLoading(true);
+
+    try {
+      const response = await axios.request
+      (options);
+
+      setData(response.data.data);
+    } catch (error) {  
+      setError(error);
+      alert('There is an error');
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  // Pour utiliser cette fonction qui va chercher les données
+  // On la déclenche dans un useEffect
+  useEffect(() => {
+    fetchData();
+  }, [])
+
+  // Il a créé cette fonction car il avait des soucis si fetch à plusieurs reprises
+  const refetch = () => {
+    setIsLoading(true);
+    fetchData();
+  }
+
+  // On renvoie tout ce dont on a besoin
+  return { data, isLoading, error, refetch };
+}
